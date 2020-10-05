97cdaec63e674069f9c4755f9de53956c1fb93b0
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-10-05 09:15:16 -0600

    Opciones con ciclo do while

--- src/menu.kt
+++ src/menu.kt
@@ -1,47 +1,76 @@
+fun elegirIngredientes() {
+    do {
+        val instrucciones = """
+            Seleccione los ingredientes que desea
+            utilizar
+        """.trimIndent()
+        val response: String? = readLine()
+
+        val ingredientes: List<String> = listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal", "Huevos", "Aceite", "Salir")
+        for (ingrediente in ingredientes) {
+            println(ingrediente)
+        }
+    } while(!response?.toLowerCase().equals("salir"))
+
+}
+
 fun main(){
-    val instrucciones = """
+    val opciones = object {
+        val hacerReceta: String = "hacer receta"
+        val verRecetas: String = "ver mis recetas"
+        val salir: String = "salir"
+        val opcion1: String = "1"
+        val opcion2: String = "2"
+        val opcion3: String = "3"
+    }
+
+    val recetas = object {
+        val mexicanas: List<String> = listOf<String>("Aguachile", "Tacos de asada", "Guacamole")
+        val japonesas: List<String> = listOf<String>("Curry", "Setas teriyaki", "Ramen")
+    }
+
+    /*if (response?.toLowerCase().equals(opciones.hacerReceta)) {
+        println("Elegir ingredientes")
+    }
+
+    if (response?.toLowerCase().equals(opciones.verRecetas)) {
+        for (receta in recetas.mexicanas.plus(recetas.japonesas)) {
+            println(receta)
+        }
+    }
+
+    if (response?.toLowerCase().equals(opciones.salir)) {
+        println("Elegir ingredientes")
+    }*/
+
+    do {
+        val instrucciones = """
         Selecciona la opción deseada, escribiendo
         el número correspondiente, o escribiendo la
         acción deseada "Hacer receta"
         1. Hacer receta
         2. Ver mis recetas
         3. Salir
-    """.trimIndent()
-    println(instrucciones)
-    val response: String? = readLine()
+        """.trimIndent()
+        println(instrucciones)
+        val response: String? = readLine()
 
-    val opciones = object {
-        val hacerReceta: String = "hacer receta"
-        val verRecetas: String = "ver mis recetas"
-        val salir: String = "Salir"
-        val opcion1: Int = 1
-        val opcion2: Int = 2
-        val opcion3: Int = 3
-    }
+        when(response?.toLowerCase()) {
+            opciones.hacerReceta -> elegirIngredientes()
+            opciones.verRecetas ->  for (receta in recetas.mexicanas.plus(recetas.japonesas)) {
+                println(receta)
+            }
+            opciones.salir -> println("Saliendo")
 
-    val ingredientes: List<String> = listOf("Agua", "Harina", "Huevos")
+            opciones.opcion1 -> elegirIngredientes()
+            opciones.opcion2 ->  for (receta in recetas.mexicanas.plus(recetas.japonesas)) {
+                println(receta)
+            }
+            opciones.opcion3 -> println("Saliendo")
 
-    val recetas = object {
-        val mexicanas: List<String> = listOf<String>("Aguachile", "Tacos de asada", "Guacamole" )
-        val japonesas: List<String> = listOf<String>("Curry", "Setas teriyaki", "Ramen" )
-
-    }
-
-    if (response?.toLowerCase().equals(opciones.hacerReceta)) {
-        println("Elegir ingredientes")
-    }
-
-    if (response?.toLowerCase().equals(opciones.verRecetas)) {
-        for (receta in recetas.mexicanas.plus(recetas.japonesas) ) {
-            println(receta)
+            else -> println("No entendí que quizo decir")
         }
-    }
-
-    if(response?.toLowerCase().equals(opciones.salir))
-
-
-
-
+    } while (!response.equals("3"))
 
 }
 
